4569d7d66407739e07af4548e6b354e0687fa008
AmitYarkoni <amityarkony9@gmail.com>
2024-01-19 23:25:14 +0200

    started implementing Collector

--- src/CollectorVolunteer.cpp
+++ src/CollectorVolunteer.cpp
@@ -1,9 +1,36 @@
-#include "../include/Volunteer.h" 
-#include "../include/Order.h" 
+#include "../include/Volunteer.h"
+#include "../include/Order.h"
 #include <string>
 #include <vector>
 using std::string;
 using std::vector;
 
-// CollectorVolunteer::CollectorVolunteer(int idP, string nameP, int coolDownP) : id(idP), name(nameP), coolDown(coolDownP),
-//  completedOrderId(0), activeOrderId(0) {}
+
+CollectorVolunteer::CollectorVolunteer(int id, const string &name, int coolDown)
+    : CollectorVolunteer(id, name, coolDown)
+{
+    // not sure if the inheritance is being done correctly.
+}
+
+CollectorVolunteer *CollectorVolunteer::clone() const
+{
+    CollectorVolunteer *a = new CollectorVolunteer(getId(), getName(), getCoolDown()); 
+    a->timeLeft = getTimeLeft();  
+    return (a);
+}
+
+bool CollectorVolunteer::hasOrdersLeft() const{
+    return true;
+}
+
+bool CollectorVolunteer::canTakeOrder(const Order &order) const{
+    return ((coolDown == 0)&&(getActiveOrderId == 0));
+}
+
+// virtual void acceptOrder(const Order &order) = 0;
+// Prepare for new order(Reset activeOrderId,TimeLeft,DistanceLeft,OrdersLeft depends on the volunteer type)
+
+void CollectorVolunteer::acceptOrder(const Order &order){
+    activeOrderId = order.getId();
+    timeLeft = coolDown;
+}
